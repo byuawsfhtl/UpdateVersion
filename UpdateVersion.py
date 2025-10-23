1c5b11dc90d84182e1bbab5e24e89fd767e57ea3
Elijah <epbay.01@gmail.com>
2025-10-23 17:56:25 -0600

    update

--- UpdateVersion.py
+++ UpdateVersion.py
@@ -8,7 +8,8 @@ def getActiveBranchName() -> str:
         str: the name of the active branch
     """
     head_dir = Path(".") / ".git" / "HEAD"
-    with head_dir.open("r") as f: content = f.read().splitlines()
+    with head_dir.open("r") as f: 
+        content = f.read().splitlines()
 
     for line in content:
         if line[0:4] == "ref:":
